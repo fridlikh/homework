261471a0832a660bb8d7010afe0fb9273ce6a10d
Leonid Fridlikh <lfridlikh@inno.tech>
2025-06-20 01:04:20 +0300

    awt.Point created

--- src/main/java/ru/stepup/main/MainApp.java
+++ src/main/java/ru/stepup/main/MainApp.java
@@ -1,12 +1,14 @@
 package ru.stepup.main;
 
-import ru.stepup.HW.OOP.math.Power;
+import ru.stepup.HW.OOP.geometry.*;
 
 
 public class MainApp {
     public static void main(String[] args) {
-        Power pow1 = new Power("2", "7");
-        System.out.println(pow1);
+        Point a = new Point(1,3);
+        java.awt.Point b = new java.awt.Point(10, 20);
+        System.out.println("Моя точка " + a);
+        System.out.println("Awt точка " + b);
 
     }
 }
